da79bf172c8aeb5a625a157b92cb954db480b6e0
Tuan Ngoc T. <105979341+HarlausButtercake@users.noreply.github.com>
2024-10-23 01:09:36 +0700

    Now with user button interrupt

--- Debug/Src/main.d
+++ Debug/Src/main.d
@@ -1,10 +1,13 @@
-Src/main.o: ../Src/main.c ../Inc/stm32f0xx.h ../Inc/stm32f030x8.h \
- ../Inc/core_cm0.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
- ../Inc/cmsis_gcc.h ../Inc/system_stm32f0xx.h
-../Inc/stm32f0xx.h:
-../Inc/stm32f030x8.h:
-../Inc/core_cm0.h:
-../Inc/cmsis_version.h:
-../Inc/cmsis_compiler.h:
-../Inc/cmsis_gcc.h:
-../Inc/system_stm32f0xx.h:
+Src/main.o: ../Src/main.c ../Header\ Files/stm32f0xx.h \
+ ../Header\ Files/stm32f030x8.h ../Header\ Files/core_cm0.h \
+ ../Header\ Files/cmsis_version.h ../Header\ Files/cmsis_compiler.h \
+ ../Header\ Files/cmsis_gcc.h ../Header\ Files/system_stm32f0xx.h \
+ ../Header\ Files/stm32f030x8.h
+../Header\ Files/stm32f0xx.h:
+../Header\ Files/stm32f030x8.h:
+../Header\ Files/core_cm0.h:
+../Header\ Files/cmsis_version.h:
+../Header\ Files/cmsis_compiler.h:
+../Header\ Files/cmsis_gcc.h:
+../Header\ Files/system_stm32f0xx.h:
+../Header\ Files/stm32f030x8.h:
